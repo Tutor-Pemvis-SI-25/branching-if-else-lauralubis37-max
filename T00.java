a5692fa6679928f0cd8841fc47cd0767fab9b642
lauralubis37-max <lubislaura08@gmail.com>
2025-10-03 14:40:30 +0700

    Pair-27
    memperbaiki

--- T00.java
+++ T00.java
@@ -12,37 +12,40 @@ public class T00 {
         x = Integer.parseInt(input.nextLine());
         y = Integer.parseInt(input.nextLine());
         if (x % 2 == 0) {
-            System.out.println("bilangan pertama genap");
+            System.out.println("Bilangan pertama: Genap");
         } else {
-            System.out.println("bilangan pertama ganjil");
+            System.out.print("Bilangan pertama: Ganjil");
         }
         if (y % 2 == 0) {
-            System.out.println("bilangan kedua genap");
+            System.out.println("Bilangan kedua: Genap");
         } else {
-            System.out.println("bilangan kedua ganjil");
+            System.out.println("Bilangan kedua: Ganjil");
         }
         if (x == y) {
-            System.out.println("kedua bilangan sama besar");
+            System.out.println("Kedua bilangan sama besar");
         } else {
             if (x < y) {
-                System.out.println("bilangan kedua lebih besar");
+                System.out.println("Bilangan kedua lebih besar");
             } else {
-                System.out.println("bilangan kedua lebih kecil");
+                System.out.println("Bilangan kedua lebih kecil");
             }
         }
         if (x % 2 == 0 && y % 2 == 0) {
             q = x + y;
-            System.out.println(q);
+            System.out.println("Hasil penjumlahan: " + q);
         } else {
             if (x % 2 != 0 && y % 2 != 0) {
                 r = x * y;
-                System.out.println(r);
+                System.out.println("Hasil perkalian: " + r);
             } else {
                 if (x % 2 == 0 && y % 2 != 0) {
-                    System.out.println("berbeda jenis");
+                    System.out.println("Berbeda jenis");
                 } else {
                     if (x % 2 != 0 && y % 2 == 0) {
-                        System.out.println("berbeda jenis");
+                        System.out.println("Berbeda jenis");
                     }
                 }
             }
+        }
+    }
+}
